cb1b43e72047fed7fa243a958491094b457d9521
Santiago Aramburu <santi.aramburu45@gmail.com>
2025-08-16 19:00:55 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -14,17 +14,3 @@ document.querySelector("form").addEventListener("submit", function(e){
 
 
 
-<script>
-  const API_KEY = '00edb26c00b5a552463408c57964037e';
-  const ciudad = 'Montevideo';
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${API_KEY}&units=metric&lang=es`;
-
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      const temp = Math.round(data.main.temp);
-      const estado = data.weather[0].description;
-      document.getElementById('temperatura').textContent = `${temp}°C`;
-      document.getElementById('estado').textContent = estado.charAt(0).toUpperCase() + estado.slice(1);
-    });
-</script>
