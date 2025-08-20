f985315e42f3fa4080900424120495fbb1d1c2af
Mateo <kjlgames1@gmail.com>
2025-08-20 09:15:58 -0300

    quitado .trim de la contraseña

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ document.querySelector("form").addEventListener("submit", function(e){
     e.preventDefault();
 
     const nombre = document.getElementById("nombre").value.trim();
-    const contraseña = document.getElementById("password").value.trim();
+    const contraseña = document.getElementById("password").value;
 
     if (nombre === "" || contraseña === "") {
         alert("Por favor, complete todos los campos.");
