c84c0527b5acedb5d7b05bc12fb73d69bfea1dd5
Mateo <kjlgames1@gmail.com>
2025-11-30 19:39:10 -0300

    reorganizacion

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,18 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function verifyToken(req, res, next) {
-    const authHeader = req.headers.authorization;
+    const token = req.headers["access-token"];
 
-    // Verificar si viene el header Authorization
-    if (!authHeader) {
+    if (!token) {
         return res.status(401).json({ error: "Token no proporcionado" });
     }
 
-    const token = authHeader.split(" ")[1]; // formato: "Bearer token"
-
     try {
         const decoded = jwt.verify(token, "CLAVE_SECRETA");
-        req.user = decoded; // guardamos los datos del token en la request
+        req.user = decoded;
         next();
     } catch (error) {
         return res.status(403).json({ error: "Token inválido o expirado" });
